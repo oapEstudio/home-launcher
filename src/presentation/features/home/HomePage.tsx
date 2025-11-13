7e8e36b99772363163760b5a5d0fde8bc1391f21
bojeda-sofredigital <bojeda@sofredigital.com.ar>
2025-11-13 11:17:53 -0300

    fix mock

--- src/presentation/features/home/HomePage.tsx
+++ src/presentation/features/home/HomePage.tsx
@@ -54,7 +54,7 @@ export const HomePage = () => {
         <Container description={'HOME'} title='HOME'>
            <CustomBox sx={{paddingLeft: '1rem', paddingRight: '2rem'}}>
               {!loadingCarousel? 
-                    <Carousel slides={mockSlides/*slides.length >0 ? slides : [slideDefault] */} height={490} /> : 
+                    <Carousel slides={slides.length >0 ? slides : [slideDefault]} height={490} /> : 
                     <CustomStack sx={{padding: '1%'}} direction='row' spacing={1}>
                       <Skeleton variant="rectangular" height={450} sx={{width: {xs: '100%',md: '50%'}}}  />
                       <Skeleton variant="rectangular" width='50%' height={450} sx={{display: {xs: 'none',md: 'block'}}}/>
